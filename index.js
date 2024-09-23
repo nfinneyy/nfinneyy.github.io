c521b63511f0d19957ac90a470579c82975cdfb3
nfinneyy <noah.finney@giveducation.co.uk>
2024-09-23 16:39:31 +0100

    portfolio site

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("avatar").addEventListener("mouseenter",(event)=>{
+    event.target.src = "https://i.pinimg.com/originals/71/df/0d/71df0d3d2968f0e8c1cf6501b480bcbb.png"
+})
+document.getElementById("avatar").addEventListener("mouseleave",(event)=>{
+    event.target.src = "https://avatars.githubusercontent.com/u/182099190"
+})
